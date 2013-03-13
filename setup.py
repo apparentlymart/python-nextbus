48be08f5695a6efa059c0faafae543f7df7ac0c2
Kyle Derkacz <kyleder@gmail.com>
2013-03-13 05:58:05 -0700

    Adding a disutils setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='python-nextbus',
+    description='A Python library for connecting to the NextBus public API',
+    version='0.1',
+    packages=['python-nextbus'],
+    url='https://github.com/apparentlymart/python-nextbus',
+    requires=[],
+)
